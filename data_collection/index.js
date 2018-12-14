dfc007893930a0ed7f7e54c3c06e65336c00f781
Pei-Ya Chiu <lockercho@gmail.com>
2018-12-14 16:03:33 +0800

    add data collector

--- data_collection/index.js
+++ data_collection/index.js
@@ -2,7 +2,7 @@ const express = require('express')
 const bodyParser = require('body-parser')
 const fs = require('fs')
 const app = express()
-const port = 3000
+const port = 7788
 
 const jokes = JSON.parse(fs.readFileSync('jokes-1213.json', {encoding:'utf8'}))
 const njokes = jokes.length
