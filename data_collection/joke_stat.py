1353d1fcfbec33a74d0cdb5bd93689575b5a7980
Pei-Ya Chiu <lockercho@gmail.com>
2018-12-18 14:42:59 +0800

    update

--- data_collection/joke_stat.py
+++ data_collection/joke_stat.py
@@ -10,12 +10,15 @@ with open('user_labels', 'r') as f:
 		for line in f:
 			line = line.strip()
 			# line = '[12/18/2018, 2:16:47 PM][1545113807] 109: 2, aaa'
-			time = line[25:35]
-			labels = line[36:].strip()
+			time = line[26:36]
+			labels = line[37:].strip()
 			print(time)
 			print(labels)
 			joke_id, other = labels.split(':')
-			img_id, name = other.split(', ')
+			toks = other.split(',')
+			if len(toks) == 2:
+				img_id, name = toks[0].strip(), toks[1].strip()
+			if len(toks) == 1:
+				img_id, name = toks[0].strip(), ''
 			print(time, joke_id, img_id, name)
-			all_labels.append((time, joke_id, img_id, name))
 			wf.write("%s,%s,%s,%s\n" % (time, joke_id, img_id, name))
